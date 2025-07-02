7f643c64a661d900c0baaaebf628bbf4690e160a
Mehdiz-dev <mehdi.zarho@gmail.com>
2025-07-02 09:57:07 +0200

    Création du signup.html pour tests & ajout dans server.js

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const supabase = require('./services/supabaseClient');
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use(cors());
 app.use(express.json());
